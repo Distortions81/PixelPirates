cb940aa9a2ea59ee673dc87bf92337144818d210
Distortions81 <carlotto81@gmail.com>
2025-01-25 20:43:15 -0700

    add some game structs

---
+++ struct.go
@@ -0,0 +1,18 @@
+package main
+
+import "image/color"
+
+type Game struct {
+	gameMode        int
+	boatPos, camPos point
+
+	colors colorData
+}
+
+type colorData struct {
+	sky, water color.RGBA
+}
+
+type point struct {
+	X, Y int
+}
