95bd49860f8656b1a84a8ada7f67ad5d7a441cd2
Distortions81 <carlotto81@gmail.com>
2025-01-26 23:15:45 -0700

    add mouse click start

--- input.go
+++ input.go
@@ -11,7 +11,9 @@ func (g *Game) Update() error {
 	pressedKeys := inpututil.AppendPressedKeys(nil)
 
 	if g.gameMode == GAME_TITLE {
-		if pressedKeys != nil {
+		if pressedKeys != nil ||
+			inpututil.IsMouseButtonJustPressed(ebiten.MouseButton0) ||
+			inpututil.IsMouseButtonJustPressed(ebiten.MouseButton1) {
 			g.gameMode = GAME_PLAY
 		}
 		return nil
