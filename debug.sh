335d014fc22149da781ed6d0dae14c3188d653a8
Distortions81 <carlotto81@gmail.com>
2025-02-04 19:07:48 -0700

    .

--- debug.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-go clean
-go build -tags=ebitenginedebug
-EBITENGINE_INTERNAL_IMAGES_KEY=P ./PixelPirates
---
+++ ebitenDebug.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+go clean
+go build -tags=ebitenginedebug
+EBITENGINE_INTERNAL_IMAGES_KEY=P ./PixelPirates
+go clean
